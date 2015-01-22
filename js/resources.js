596e73721cdc770001a9d9801796900449cb9487
Brandon Nguyen <eagleeye2555@gmail.com>
2015-01-22 11:01:06 -0800

    Finished up to video 5 where i got to load the map.

--- js/resources.js
+++ js/resources.js
@@ -4,6 +4,10 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
+	 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+	 //graphics for Awesomenauts that the game would use.
+
 
 	/* Atlases 
 	 * @example
@@ -15,6 +19,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
+ 	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},
+ 	 //maps for our game
 
 	/* Background music. 
 	 * @example
