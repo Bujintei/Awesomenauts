e355370079fbdf006534c171febba30ba9dc63aa
Brandon Nguyen <eagleeye2555@gmail.com>
2015-03-11 10:57:05 -0700

    Video 43 Code : Adding Text

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -12,7 +12,13 @@ game.SpendExp = me.ScreenObject.extend({
 			},
 
 			draw: function(renderer) {
-				this.font.draw(renderer.getContext(), "SPEND", this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), "CURRENT EXP: " + me.save.exp.toString(), this.pos.x + 100, this.pos.y + 50);
+				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION " + me.save.exp.toString(), this.pos.x + 200, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD " + me.save.exp.toString(), this.pos.x + 200, this.pos.y + 150);
+				this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE " + me.save.exp.toString(), this.pos.x + 200, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH " + me.save.exp.toString(), this.pos.x + 200, this.pos.y + 250);
+
 			}
 
 		})));
